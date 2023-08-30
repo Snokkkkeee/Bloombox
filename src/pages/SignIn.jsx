d667105c8783411dfb46d11251139c176245d915
gavinprinsloo <prinsloogavin01@gmail.com>
2023-08-30 23:03:09 +0200

    SignIn and SignUp, Needs Fix

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,32 +1,45 @@
-import React, { useState } from 'react';
-import { Form, Input, Button, Layout, Typography } from 'antd';
-import { UserOutlined, LockOutlined, GoogleOutlined, GithubOutlined } from '@ant-design/icons';
-import { useNavigate } from 'react-router-dom';
-import logo from '/Users/jwsnooke/Desktop/Bloombox Layout/src/assets/GrowBox.png';
-import backgroundImage from '/Users/jwsnooke/Desktop/Bloombox Layout/src/assets/c71c3bbd-6268-43da-aa49-14ce1d1700f1.png';
+import React, { useState } from "react";
+import { Form, Input, Button, Layout, Typography } from "antd";
+import {
+  UserOutlined,
+  LockOutlined,
+  GoogleOutlined,
+  GithubOutlined,
+} from "@ant-design/icons";
+import { Navigate, useNavigate } from "react-router-dom";
+import { signIn } from "../Services/auth"; // Assuming you have the signIn function defined here
+import logo from "../assets/GrowBox.png";
+import backgroundImage from "../assets/c71c3bbd-6268-43da-aa49-14ce1d1700f1.png";
 
 const { Content } = Layout;
 const { Title, Text } = Typography;
 
 const inputStyle = {
-  width: '100%',
-  padding: '10px',
-  borderRadius: '10px',
-  color: '#333',
+  width: "100%",
+  padding: "10px",
+  borderRadius: "10px",
+  color: "#333",
 };
 
-export default function SignIn({ authenticate }) {
+export default function SignIn() {
   const [redirectToDashboard, setRedirectToDashboard] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
- const navigate = useNavigate(); 
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
+
   const onFinish = async (values) => {
-     const { user, errorMessage } = await signIn( 
-    values.username,
+    const { user, errorMessage } = await signIn(
+      values.username,
       values.password
     );
 
     if (user) {
-      setRedirectToDashboard(true);
+      // Assuming you have a user ID or some unique identifier in the user object
+      // This check ensures that only registered users can sign in
+      if (user.userId) {
+        setRedirectToDashboard(true);
+      } else {
+        setErrorMessage("Invalid credentials. Please try again.");
+      }
     } else {
       setErrorMessage(errorMessage);
     }
@@ -40,45 +53,45 @@ export default function SignIn({ authenticate }) {
     <Layout
       style={{
         backgroundImage: `url(${backgroundImage})`,
-        backgroundSize: 'cover',
-        minHeight: '100vh',
-        minWidth: '50vw',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
+        backgroundSize: "cover",
+        minHeight: "100vh",
+        minWidth: "50vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
       <Content
         style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          padding: '50px',
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "50px",
         }}
       >
         <div
           style={{
-            maxWidth: '1000px',
-            background: 'rgba(255, 255, 255, 0.9)',
-            padding: '50px',
-            borderRadius: '25px',
-            textAlign: 'center',
-            margin: '20px',
+            maxWidth: "1000px",
+            background: "rgba(255, 255, 255, 0.9)",
+            padding: "50px",
+            borderRadius: "25px",
+            textAlign: "center",
+            margin: "20px",
           }}
         >
-          <Title level={2} style={{ color: '#2accb6', marginBottom: '20px' }}>
-            Connect to{' '}
+          <Title level={2} style={{ color: "#2accb6", marginBottom: "20px" }}>
+            Connect to{" "}
             <img
               src={logo}
-              alt="BloomBox"
+              alt="GrowBox"
               style={{
-                width: '200px',
-                verticalAlign: 'middle',
-                marginBottom: '20px',
+                width: "200px",
+                verticalAlign: "middle",
+                marginBottom: "20px",
               }}
             />
           </Title>
-          <Text type="secondary" style={{ marginBottom: '30px' }}>
+          <Text type="secondary" style={{ marginBottom: "30px" }}>
             Sign in to continue to your dashboard
           </Text>
 
@@ -87,17 +100,25 @@ export default function SignIn({ authenticate }) {
             className="login-form"
             initialValues={{ remember: true }}
             onFinish={onFinish}
-            style={{ marginTop: '30px' }}
+            style={{ marginTop: "30px" }}
           >
             <Form.Item
               name="username"
-              rules={[{ required: true, message: 'Please input your Username!' }]}
+              rules={[
+                { required: true, message: "Please input your Username!" },
+              ]}
             >
-              <Input prefix={<UserOutlined />} placeholder="Username/Email" style={inputStyle} />
+              <Input
+                prefix={<UserOutlined />}
+                placeholder="Username/Email"
+                style={inputStyle}
+              />
             </Form.Item>
             <Form.Item
               name="password"
-              rules={[{ required: true, message: 'Please input your Password!' }]}
+              rules={[
+                { required: true, message: "Please input your Password!" },
+              ]}
             >
               <Input
                 prefix={<LockOutlined />}
@@ -107,7 +128,7 @@ export default function SignIn({ authenticate }) {
               />
             </Form.Item>
             {errorMessage && (
-              <Text type="danger" style={{ marginBottom: '15px' }}>
+              <Text type="danger" style={{ marginBottom: "15px" }}>
                 {errorMessage}
               </Text>
             )}
@@ -116,12 +137,12 @@ export default function SignIn({ authenticate }) {
                 type="primary"
                 htmlType="submit"
                 style={{
-                  width: '80%',
-                  height: '40px',
-                  fontSize: '16px',
-                  borderRadius: '15px',
-                  border: 'none',
-                  marginBottom: '10px',
+                  width: "80%",
+                  height: "40px",
+                  fontSize: "16px",
+                  borderRadius: "15px",
+                  border: "none",
+                  marginBottom: "10px",
                 }}
               >
                 Log in
@@ -132,14 +153,14 @@ export default function SignIn({ authenticate }) {
           <Button
             icon={<GoogleOutlined />}
             style={{
-              width: '60%',
-              height: '40px',
-              fontSize: '16px',
-              backgroundColor: '#2accb6',
-              color: 'white',
-              borderRadius: '15px',
-              border: 'none',
-              marginBottom: '15px',
+              width: "60%",
+              height: "40px",
+              fontSize: "16px",
+              backgroundColor: "#2accb6",
+              color: "white",
+              borderRadius: "15px",
+              border: "none",
+              marginBottom: "15px",
             }}
           >
             Sign in with Google
@@ -147,17 +168,32 @@ export default function SignIn({ authenticate }) {
           <Button
             icon={<GithubOutlined />}
             style={{
-              width: '60%',
-              height: '40px',
-              fontSize: '16px',
-              backgroundColor: '#333',
-              color: 'white',
-              borderRadius: '15px',
-              border: 'none',
-              marginBottom: '15px',
+              width: "60%",
+              height: "40px",
+              fontSize: "16px",
+              backgroundColor: "#333",
+              color: "white",
+              borderRadius: "15px",
+              border: "none",
+              marginBottom: "15px",
             }}
           >
             Sign in with GitHub
+          </Button>
+          <Button
+            type="link"
+            onClick={() => navigate("/SignUp")}
+            htmlType="submit"
+            style={{
+              width: "80%",
+              height: "40px",
+              fontSize: "16px",
+              borderRadius: "15px",
+              border: "none",
+              marginBottom: "10px",
+            }}
+          >
+            Register
           </Button>
         </div>
       </Content>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,81 +1,164 @@
-import React, { useState } from 'react';
-import { Form, Input, Button, Layout, Typography, Checkbox, Tooltip, Row, Col } from 'antd';
-import { UserOutlined, GoogleOutlined, GithubOutlined } from '@ant-design/icons';
-import { Link, useNavigate } from 'react-router-dom';
-import logo from '../assets/GrowBox.png';
-import backgroundImage from '../assets/c71c3bbd-6268-43da-aa49-14ce1d1700f1.png';
-import Styled from "styled-components"
+import React, { useState } from "react";
+import {
+  Form,
+  Input,
+  Button,
+  Layout,
+  Typography,
+  Checkbox,
+  Row,
+  Col,
+  Card,
+} from "antd";
+import { Link, useNavigate } from "react-router-dom";
+import backgroundImage from "../assets/c71c3bbd-6268-43da-aa49-14ce1d1700f1.png";
+import { signUp } from "../Services/auth";
+import Styled from "styled-components";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth, db } from "../Services/firebase";
+import { setDoc, doc } from "firebase/firestore";
+
 const { Content } = Layout;
 const { Title, Text } = Typography;
 
+const StyledFormItem = Styled(Form.Item)`
+  /* Your styled component styles here */
+`;
 
+const buttonStyle = {
+  width: "80%",
+  height: "40px",
+  fontSize: "16px",
+  borderRadius: "15px",
+  border: "none",
+  marginBottom: "10px",
+};
 
 export default function SignUp() {
-  const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
   const navigate = useNavigate();
 
   const onFinish = async (values) => {
     try {
-      // Replace with your signUp logic
-      await signUp({
+      // Create user in Firebase Authentication
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        values.email,
+        values.password
+      );
+
+      // Create a document in Firestore with the user's data
+      await setDoc(doc(db, "users", userCredential.user.uid), {
         email: values.email,
-        password: values.password,
         nickname: values.nickname,
       });
 
-      navigate('/SignIn');
+      navigate("/");
     } catch (error) {
-      setErrorMessage('Unable to sign up. Please try again later.');
+      console.error("Error during sign up:", error);
+      setErrorMessage("Unable to sign up. Please try again later.");
     }
   };
 
+  const onFinishFailed = (errorInfo) => {
+    console.log("Failed:", errorInfo);
+  };
+
   return (
-    <Layout style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover', minHeight: '100vh' }}>
-      <Content style={{ padding: '0 50px' }}>
-        <div className="site-layout-content">
-          <Row justify="center">
-            <Col span={16}>
-              <Title style={{ color: 'white' }}>Sign Up</Title>
-              <p>Use these awesome forms to login or create a new account in your project for free.</p>
-              <Card title="Register With">
-                <Form
-                  name="basic"
-                  initialValues={{ remember: true }}
-                  onFinish={onFinish}
-                  onFinishFailed={onFinishFailed}
-                >
-                  <StyledFormItem
-                    label="Email"
-                    name="email"
-                    rules={[
-                      {
-                        required: true,
-                        message: 'Please input your email!',
-                        type: 'email',
-                      },
-                    ]}
-                  >
-                    <Input />
-                  </StyledFormItem>
-                  <StyledFormItem
-                    label="Password"
-                    name="password"
-                    rules={[
-                      {
-                        required: true,
-                        message: 'Please input your password!',
-                      },
-                    ]}
-                  >
-                    <Input.Password />
-                  </StyledFormItem>
-                  <Form.Item name="remember" valuePropName="checked">
-                    <Checkbox>Remember me</Checkbox>
-                  </Form.Item>
-                </Form>
-              </Card>
-            </Col>
-          </Row>
+    <Layout
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        minHeight: "100vh",
+        minWidth: "50vw",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Content
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          padding: "50px",
+        }}
+      >
+        <div
+          style={{
+            maxWidth: "1000px",
+            background: "rgba(255, 255, 255, 0.9)",
+            padding: "50px",
+            borderRadius: "25px",
+            textAlign: "center",
+            margin: "20px",
+          }}
+        >
+          <Title level={2} style={{ color: "#2accb6", marginBottom: "20px" }}>
+            Sign Up
+          </Title>
+          <Text type="secondary" style={{ marginBottom: "30px" }}>
+            Use these awesome forms to sign up for free.
+          </Text>
+
+          <Form
+            name="basic"
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            style={{ marginTop: "30px" }}
+          >
+            <StyledFormItem
+              label="Email"
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your email!",
+                  type: "email",
+                },
+              ]}
+            >
+              <Input />
+            </StyledFormItem>
+            <StyledFormItem
+              label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your password!",
+                },
+              ]}
+            >
+              <Input.Password />
+            </StyledFormItem>
+            <StyledFormItem
+              label="Nickname"
+              name="nickname"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your nickname!",
+                },
+              ]}
+            >
+              <Input />
+            </StyledFormItem>
+            <Form.Item name="remember" valuePropName="checked">
+              <Checkbox>Remember me</Checkbox>
+            </Form.Item>
+            <Form.Item>
+              <Button type="primary" htmlType="submit" style={buttonStyle}>
+                Sign Up
+              </Button>
+            </Form.Item>
+          </Form>
+          {errorMessage && (
+            <Text type="danger" style={{ marginBottom: "15px" }}>
+              {errorMessage}
+            </Text>
+          )}
         </div>
       </Content>
     </Layout>
